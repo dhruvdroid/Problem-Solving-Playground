ad189ede9c1576ae04c2dbc297c2ce8ce1dd8778
Dhruv <cr.dhruvsingh@gmail.com>
2020-10-06 22:59:28 +0530

    add two linklist in progress

---
+++ src/leetcode/java/AddTwoNumber.java
@@ -0,0 +1,67 @@
+package leetcode.java;
+
+
+/**
+ * Link Problem - https://leetcode.com/problems/add-two-numbers/
+ * <p>
+ * You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+ * <p>
+ * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * <p>
+ * Input: l1 = [2,4,3], l2 = [5,6,4]
+ * Output: [7,0,8]
+ * Explanation: 342 + 465 = 807.
+ * Example 2:
+ * <p>
+ * Input: l1 = [0], l2 = [0]
+ * Output: [0]
+ * Example 3:
+ * <p>
+ * Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+ * Output: [8,9,9,9,0,0,0,1]
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * The number of nodes in each linked list is in the range [1, 100].
+ * 0 <= Node.val <= 9
+ * It is guaranteed that the list represents a number that does not have leading zeros.
+ */
+
+public class AddTwoNumber {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static void main(String[] args) {
+
+
+
+
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        return null;
+
+    }
+}
